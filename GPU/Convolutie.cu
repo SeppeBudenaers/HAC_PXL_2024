36eab01681ad5ce16be9b03b0cd9b06c86d5ab40
seppe budenaers <101107875+SeppeBudenaers@users.noreply.github.com>
2024-03-19 21:13:10 +0100

    Update Convolutie.cu

--- GPU/Convolutie.cu
+++ GPU/Convolutie.cu
@@ -86,6 +86,7 @@ int main(int argc, char* argv[]) {
         dim3 gridSize((width + BLOCK_SIZE - 1) / BLOCK_SIZE, (height + BLOCK_SIZE - 1) / BLOCK_SIZE);
     
         applyConvolution<<<gridSize, blockSize>>>(d_img, d_outputImg, width, height, channels, d_kernel);
+        cudasyncronize();
         cudaMemcpy(outputImg, d_outputImg, width * height * channels, cudaMemcpyDeviceToHost);
     
     stop =clock();
